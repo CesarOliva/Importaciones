5dfa260b27f1be358c513f27bd2056e566afc0cb
cesar <cesaroliva.work@gmail.com>
2025-09-30 15:31:13 -0600

    Agregando testimonios

---
+++ main.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      768: { slidesPerView: 2 },
+      1024: { slidesPerView: 3 }
+    }
+});
